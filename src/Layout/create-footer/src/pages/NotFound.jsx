7afa104feeea70d31d20deaded9d7b79405f5ff8
Andres_Berrio <andrewsolutions81@gmail.com>
2022-09-12 17:14:22 -0500

    feature every branch in layout folder in source

---
+++ src/Layout/create-footer/src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="notfound-container">
+      <p>Not Found</p>
+    </div>
+  )
+}
+
+export default NotFound;
---
+++ src/Layout/create-home/src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LanguageButton(){
+  return(
+    <div className="LanguageButton-container">
+      <button className='LanguageButton__button'>
+        <img src="../../../public/media/globe-icon.png" alt="globe-icon" />
+      </button>
+    </div>
+  )
+}
+
+export default LanguageButton;
