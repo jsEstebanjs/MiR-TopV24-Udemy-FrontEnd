b27cefceaf29917e6747f034cfd826dd74fc3236
Andres Berrio (Andrew) <97372501+andrewsolutions81@users.noreply.github.com>
2022-09-12 14:07:49 -0500

    feature create pages folder, header component (#2)

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header'
+
+function Home(){
+  return(
+    <div className="home-container">
+      <Header />
+      <p>Home</p>
+    </div>
+  )
+}
+
+export default Home;
