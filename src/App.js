59ae4354d6c79cb3056a73e184d2484e35d35dfb
Andres_Berrio <andrewsolutions81@gmail.com>
2022-09-12 14:04:27 -0500

    feature create pages folder, header component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import {
+  React, BrowserRouter, Routes, Route,
+} from 'react-router-dom';
+import Home from './pages'
+import NotFound from './pages/NotFound'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
