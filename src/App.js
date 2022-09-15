bc5629ea863073d6d6adc9da466a23659dce30db
Maria <zeldacamila0913@gmail.com>
2022-09-14 23:45:02 -0500

    Add login form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {
 import NotFound from './pages/NotFound';
 import Home from './pages/Home'
 import SignUp from './pages/SignUp';
+import LogIn from './pages/LogIn';
+import ForgotPassword from './pages/ForgotPassword';
 import './App.css';
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
           <Route path="*" element={<NotFound />} />
           <Route path="/" element={<Home />} />
           <Route path="/join/signup/" element={<SignUp />} />
+          <Route path="/join/login/" element={<LogIn />} />
+          <Route path="/join/forgot-password/" element={<ForgotPassword />} />
         </Routes>
       </BrowserRouter>
     </div>
