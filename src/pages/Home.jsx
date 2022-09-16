d46f91ddbe94739adcb1192d9656c8ad2a6b5f9b
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-09-15 23:14:22 -0500

    imagen header terminada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Nav from '../components/Nav';
+import TitleHeading from '../components/TitleHeading';
+import headingImg from '../images/Title-heading.png';
+import headingImgPhone from '../images/Title-heading-movil.png';
 
 function Home(){
   return(
     <div className="home-container">
       <Nav 
-      login={false}
+        login={false}
       />
-      <Header />
-      <p>Home</p>
+      <TitleHeading 
+        title='Last day for these savings!'
+        text='Courses that meet today’s challenges (and tomorrow’s). They start at just COL$39,900. Sale ends today.'
+        img={headingImg}
+        alt='illustration'
+        imgPhone={headingImgPhone}
+      />
       <Footer />
     </div>
   )
