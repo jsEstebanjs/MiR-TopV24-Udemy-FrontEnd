af71d836460e8c1c0f614aadc96c35b00414865d
Andres Berrio (Andrew) <97372501+andrewsolutions81@users.noreply.github.com>
2022-09-27 08:08:32 -0500

    Feat/signup css (#17)
    * create store example
    
    * fix signup css

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,9 +4,12 @@ import Footer from "../components/Footer";
 import './SignUp.styles.css'
 
 function SignUp() {
+  const loginLink = <a href={('./login')}>log in</a>
+
 /*   const handleSubmit = (e)=>{
     e.preventDefault();
-    const {fullnam, email, password } = form;
+    const {fullname, email, password } = form;
+    console.log(form)
   } */
   return(
     <>
@@ -15,13 +18,56 @@ function SignUp() {
         <div className="signup-container">
           <h2 className="signup__title">Sign up and start learning</h2>
           <form className="signup__form"/* onSubmit={hangleSignup} */>
-            <div className="form__square">
+            <div className="form__container">
               <div className="form__square--item">
                 <h4 className="formsignup__title">Full Name</h4>
-                <input className="udlite-compact-form-control-container-1" area-invalid="false" name="fullname" required minLength={2} maxLength={64} type="text"></input>
+                <input className="signup-imput signmup-imput-name"
+                  area-invalid="false"
+                  name="fullname"
+                  minLength={2}
+                  maxLength={64}
+                  type="text"
+                  required>
+                </input>
+              </div>
+              <div className="form__square--item">
+                <h5 className="formsignup__title">Email</h5>
+                <input className="signup-imput signmup-imput-mail"
+                  aria-invalid="false"
+                  name="email"
+                  minlength="7"
+                  maxlength="64"
+                  type="email"
+                  value=""
+                  required
+                  >
+                </input>
+              </div>
+              <div className="form__square--item">
+                <h4 className="formsignup__title">Password</h4>
+                <input className="signup-imput signmup-imput-mail"
+                  aria-invalid="false"
+                  minlength="6"
+                  maxlength="64"
+                  name="password"
+                  required
+                  type="password"
+                  value="">
+                </input>
+              </div>
+              <div className="signup-lines">- - - -</div>
+              <div className="signup-aditional-info">
+                <input type="checkbox" className="special-offers-checkbox"/>
+                <p className="special-offers-text">Send me special offers, personalized recomendations,
+                 and learning tips</p>
               </div>
             </div>
+              <button type="button" className="singup-button">Sign up</button>
           </form>
+          <hr />
+          <div className="redirect-login__container">
+            <span className="redirect-login">Already have an account?{loginLink}</span>
+          </div>
         </div>
       </div>
     <Footer />
