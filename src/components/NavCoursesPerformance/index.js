4604c46e16204bcb1e13b3875ca4d43f5e1d60c9
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-09-30 22:23:21 -0500

    version lateral 3

--- src/components/NavCoursesPerformance/index.js
+++ src/components/NavCoursesPerformance/index.js
@@ -7,7 +7,7 @@ function NavCoursesPerformance(){
         <div className='main-container-nav-courses-performance'>
             <Link to='/' className='link-img-nav-courses-performance'><img  src={udemy} alt='logo-udemy' className='img-nav-courses-performance'/></Link>
             <ul className='ul-nav-courses-performance'>
-                <li className='li-nav-courses-performance'><Link to='/' className='link-nav-courses-performance'><MdOndemandVideo /><p className='item-nav-courses-performance'>Courses </p></Link></li>
+                <li className='li-nav-courses-performance border-left-courses-performance'><Link to='/' className='link-nav-courses-performance'><MdOndemandVideo /><p className='item-nav-courses-performance'>Courses </p></Link></li>
                 <li className='li-nav-courses-performance'><Link to='/' className='link-nav-courses-performance'><MdBarChart /><p className='item-nav-courses-performance'>Performance</p></Link></li>
             </ul>
         </div>
