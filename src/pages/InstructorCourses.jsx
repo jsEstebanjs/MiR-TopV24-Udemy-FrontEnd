33164089745be9af39052ba8b599311ca6ca5abb
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-10-13 17:27:39 -0500

    add loading courses

--- src/pages/InstructorCourses.jsx
+++ src/pages/InstructorCourses.jsx
@@ -4,10 +4,26 @@ import EditCourseInstructor from "../components/EditCourseInstructor";
 import { useSelector, useDispatch } from 'react-redux';
 import { SetCourses } from '../store/InstructorCourses.Slice';
 import {v4 as uuidv4} from 'uuid';
+import { useState,useEffect } from "react";
+import LoaderCreateCourse from "../components/LoaderCreateCourse";
+import axios from 'axios';
 
 function InstructorCourses(){
     const { courses } = useSelector((state)=> state.InstructorCourses);
     const dispatch = useDispatch()
+
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        axios.get(`https://dummyjson.com/comments`)
+          .then((res) => {
+            // console.log(res.data)
+          }).catch((err) => {
+            alert(`ups hay un error ${err.message}, comuniquese a servicio al cliente 31054897466`)
+          }).finally(() => {
+            setLoading(false)
+          })
+      }, [])
 
     return(
         <>
@@ -18,7 +34,11 @@ function InstructorCourses(){
                 <Link className="link-btn-instructor-courses-page" onClick={()=> dispatch(SetCourses({ id:uuidv4(), title:"" }))} >New course</Link>
             </div>
             <div className="container-edit-course-instructor">
-            {  
+            { 
+            loading
+            ?
+            < LoaderCreateCourse />
+            :
             courses.map((item)=>{
                 return(
                     <EditCourseInstructor key={item.id}/>
--- src/store/CourseLearners.Slice.js
+++ src/store/CourseLearners.Slice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { act } from 'react-dom/test-utils';
 import {v4 as uuidv4} from 'uuid';
 
 const initialState = {
