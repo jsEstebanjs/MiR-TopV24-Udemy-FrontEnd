514ba0f10c10450814b9238b50c0c6e46e513290
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-10-19 11:08:35 -0500

    fix padding nav courses

--- src/components/NavCoursesPerformance/index.js
+++ src/components/NavCoursesPerformance/index.js
@@ -9,8 +9,8 @@ function NavCoursesPerformance(){
         <div className='main-container-nav-courses-performance'>
             <Link to='/instructor/courses' className='link-img-nav-courses-performance'><img  src={udemy} alt='logo-udemy' className='img-nav-courses-performance'/></Link>
             <ul className='ul-nav-courses-performance'>
-                <li   className={pathname === "/instructor/courses" ? "li-nav-courses-performance border-left-courses-performance" : "li-nav-courses-performance"} ><Link to='/instructor/courses' className='link-nav-courses-performance'><MdOndemandVideo /><p className='item-nav-courses-performance'>Courses </p></Link></li>
-                <li   className={pathname === "/instructor/performance/overview" ? "li-nav-courses-performance border-left-courses-performance" : "li-nav-courses-performance"} ><Link to='/instructor/performance/overview' className='link-nav-courses-performance'><MdBarChart /><p className='item-nav-courses-performance'>Performance</p></Link></li>
+                <li  className={pathname === "/instructor/courses" ? "li-nav-courses-performance border-left-courses-performance" : "li-nav-courses-performance"} ><Link to='/instructor/courses' className='link-nav-courses-performance'><MdOndemandVideo /><p className='item-nav-courses-performance'>Courses </p></Link></li>
+                <li  className={pathname === "/instructor/performance/overview" ? "li-nav-courses-performance border-left-courses-performance" : "li-nav-courses-performance"} ><Link to='/instructor/performance/overview' className='link-nav-courses-performance'><MdBarChart /><p className='item-nav-courses-performance'>Performance</p></Link></li>
             </ul>
         </div>
 
