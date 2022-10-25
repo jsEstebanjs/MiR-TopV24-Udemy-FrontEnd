fcc39f6ee23c5e3251220cdf3ae107582d635f9e
Maria <zeldacamila0913@gmail.com>
2022-10-25 11:27:13 -0500

    updated modal an nav

--- src/components/Nav/IsTeacheModel.jsx
+++ src/components/Nav/IsTeacheModel.jsx
@@ -1,14 +1,22 @@
 import { useDispatch , useSelector} from 'react-redux';
 import ChangeIsInstructor from './ChangeIsInstructor';
+import { useNavigate } from "react-router-dom";
 
 function IsTeacherModal({ handleIsTeacherModal }){
     const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const handleClickTeachOnUdemy = () => {
+        dispatch(ChangeIsInstructor())
+        navigate('/instructor/courses')   
+    }
+
     return(
         <>
         <div className="opacity-manage"></div> 
         <div className="main-container-isteacher-modal">
             <p className="title-class-edit">Do you want to become and instructor?</p>
-            <button onClick={()=> dispatch(ChangeIsInstructor())}  type="button">Yes</button>
+            <button onClick={handleClickTeachOnUdemy}  type="button">Yes</button>
             <button   type="button">Maybe later</button>
         </div>
 
