172dd423fc2c416224835cd666986f8e94b878b2
BorisLeightonI <13650753+BorisLeightonI@users.noreply.github.com>
2022-10-25 11:26:09 -0500

    added auth0 sample to App (#72)

--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {store} from './store/store'
 import { Provider } from 'react-redux'
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
+    <Auth0Provider 
+            domain={process.env.REACT_APP_AUTH0_DOMAIN}
+            clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+            redirectUri={window.location.origin}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
