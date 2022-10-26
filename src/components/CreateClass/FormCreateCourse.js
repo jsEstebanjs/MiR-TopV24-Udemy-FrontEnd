7817f0a06313d07206f9893aefc84df0f36e14f8
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-10-26 13:38:29 -0500

    add-redux-to-class

--- src/components/CreateClass/FormCreateCourse.js
+++ src/components/CreateClass/FormCreateCourse.js
@@ -2,9 +2,17 @@ import InputTitleLanding from "../InputTItleLanding/index";
 import ReactQuill from 'react-quill';
 import PromotionalSource from "../PromotionalSources/index";
 import { MdClose } from "react-icons/md";
+import { useDispatch } from 'react-redux';
+import { SetClassTitle , SetClassDescription } from '../../store/CreateCourse.Slice';
+import { useState } from "react";
 
-function FormCreateCourse(){
+
+function FormCreateCourse({titleClass,id,description,urlVideo,isActive}){
+    const dispatch = useDispatch()
+    const [value,setValue] = useState("");
+    const [length,setLegth] = useState(80);
     
+
     const modules = {
         toolbar: [
           ['bold', 'italic'],
@@ -13,19 +21,28 @@ function FormCreateCourse(){
       }
 
       const mediator = (e)=>{
-        // setValue(e)
-        // dispatch(SetDescription(e))
+        setValue(e)
+        dispatch(SetClassDescription({_id:id,classDescription:e}))
       }
 
 
     return(
         
       <div className="main-container-form-create-course">
-        <InputTitleLanding   id='titleClass' limitNum={80} place='Enter a Title.'>Title Class</InputTitleLanding>    
+        <label htmlFor="titleClass" className='label-input-landing'>Title Class</label>
+        <div className="main-container-input-learn container-input-landing">
+        <input  value={titleClass} id="titleClass" className="input-input-learn" maxLength={80} type='text' placeholder='Enter a Title.' onChange={(e)=> {
+            setLegth(80 - e.target.value.length)
+            dispatch(SetClassTitle({_id:id,classTitle:e.target.value}))
+            }}/>
+
+        <span className="num-length-input-learn">{length}</span>
+
+        </div>
         <p className="label-input-landing">Class description</p>   
         <ReactQuill 
            // theme="snow"
-           // value={state.description}
+           value={description}
            placeholder="Add a description of your class."
            onChange={(e)=>{
                mediator(e)
