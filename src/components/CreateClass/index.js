05edfcf726ac21c5fa4fc044ac14e25090718890
jsEstebanjs <101421451+jsEstebanjs@users.noreply.github.com>
2022-10-26 18:21:40 -0500

    add-action (#92)

--- src/components/CreateClass/index.js
+++ src/components/CreateClass/index.js
@@ -2,10 +2,9 @@ import ModalClass from "./ModalClass"; /* create class */
 import { DragDropContext ,Droppable , Draggable} from '@hello-pangea/dnd';
 import { useDispatch , useSelector} from "react-redux";
 import { FaBars } from "react-icons/fa";
-import { useState } from "react";
-import FormCreateCourse from "./FormCreateCourse";
 import { Reorder } from "../../store/CreateCourse.Slice";
-
+import FormCreateCourse from "./FormCreateCourse";
+import { useState } from "react";
 
 
 const reorder = (list, startIndex, endIndex) => {
@@ -17,30 +16,23 @@ const reorder = (list, startIndex, endIndex) => {
 
 function CreateClass(){
 
-  const [isVisible, setIsVisible] = useState(false)
-  const handleNewCourse = ()=> {
-    setIsVisible((isVisible) => !isVisible)
+  const [isVisible,setIsVisible]=useState(false)
+
+  const handleVisible = ()=>{
+    setIsVisible(!isVisible)
   }
-
-
-
-   
-      
 
     const dispatch = useDispatch()
     const classesFromCourse = useSelector((state) => state.CreateCourse.classes)
-    console.log("clases",classesFromCourse)
 
     return(
 
       <div className="main-container-create-class">
-      {isVisible
-      ? <FormCreateCourse isDelete={false}/>
-      : null
-      }
+        {isVisible ? <FormCreateCourse  isDelete={false}  isNew={true}/> : null}
+
         <div className="container-btn-title-create-course">
           <h3 className="create-class-title">Your classes</h3>
-          <button type="button" onClick={handleNewCourse} className="create-class-btn">Create class</button>3 className="create-class-title">Your classes</h3>
+          <button onClick={ handleVisible } type="button" className="create-class-btn">Create class</button>
         </div>
         <DragDropContext onDragEnd={(result)=>{
           const { source,destination } = result;
@@ -97,63 +89,12 @@ function CreateClass(){
               </Droppable>
         </DragDropContext>
 
+
+
+
       </div>
-      <DragDropContext onDragEnd={(result)=>{
-        const { source,destination } = result;
-        if(!destination){
-          return;
-        }
 
-        if(source.index === destination.index && source.droppableId === destination.droppableId){
-          return;
-        }
-        // const items = reorder(clasess , source.index , destination.index)
-
-        // dispatch(Reorder({items}))
-      }
-
-      } >
-          <Droppable droppableId="class">
-            {
-              (droppableProvided)=>(
-                <div {...droppableProvided.droppableProps}
-                  className="container-class-edit"
-                  ref={droppableProvided.innerRef}
-                  >
-                  {classesFromCourse.map((item,index)=>(
-                    <Draggable key={item} draggableId={item} index={index}>
-                      {(draggableProvided)=>(
-                        <div
-                        className="main-container-class-edit-modal"
-                        {...draggableProvided.draggableProps}
-                        ref={draggableProvided.innerRef}
-
-                        >
-
-                        <ModalClass
-                        />
-                        <span
-                        className="btn-class-edit-modal"
-                        {...draggableProvided.dragHandleProps}
-                        ><FaBars /></span>
-                        </div>
-                  )}
-                    </Draggable>
-                  ))}
-
-                {droppableProvided.placeholder}
-                </div>
-              )
-            }
-            </Droppable>
-      </DragDropContext>
-
-
-
-
-    </div>
-
-  )
+    )
 }
 
 export default CreateClass;
