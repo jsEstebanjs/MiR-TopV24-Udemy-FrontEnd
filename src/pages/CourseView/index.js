065436774ac7070b08e1c12e9f1a59152cedf0da
jsEstebanjs <101421451+jsEstebanjs@users.noreply.github.com>
2022-11-23 12:15:56 -0500

    Fix loggin and register (#114)
    * finalizado animacion
    
    * new form register and login

--- src/pages/CourseView/index.js
+++ src/pages/CourseView/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import Accordion from 'react-bootstrap/Accordion';
 import { useLocation, useSearchParams } from 'react-router-dom';
 import udemy from '../../images/udemy.png';
 import './index.css';
@@ -73,42 +72,6 @@ export default CourseView;
 function BasicExample(classes, setVideoSrc) {
   return (
     <>
-    <Accordion >
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-
-    <Accordion >
-    {!classes?null:classes.map((clas) => 
-      <Accordion.Item eventKey={clas._id} onClick={()=>setVideoSrc(clas.classVideo)}>
-        <Accordion.Header>{clas.classTitle}</Accordion.Header>
-        <Accordion.Body>{clas.classDescription}</Accordion.Body>
-      </Accordion.Item>
-      )
-    }
-    </Accordion>
     </>
   );
 }
--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,57 +1,35 @@
-import { useForm } from "react-hook-form";
-import { Link, Navigate } from "react-router-dom";
-import { useState, useEffect  } from "react";
+import { Link } from "react-router-dom";
+import { useEffect  } from "react";
 import React from "react";
 import Nav from "../../components/Nav/index";
 import Footer from "../../components/Footer";
 import image1 from "../../images/facebook.png";
 import image2 from "../../images/buscar.png";
 import image3 from "../../images/logotipo-de-apple.png";
-import { FaEnvelope } from "react-icons/fa";
-import { HiLockClosed } from "react-icons/hi";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+import FormRegisterAndLogin from "../../components/FormRegisterAndLogin";
 
 const FormularioLogIn = () => {
   const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
   useEffect(() => {
     document.title = "Log In | Udemy";
   }, []);
-  // const {
-  //   register,
-  //   formState: { errors },
-  //   handleSubmit,
-  // } = useForm();
 
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-  });
-  const [infoUser, setInfoUser] = useState(undefined)
-  const [logged, setLogged] = useState(false)
   const navigate = useNavigate()
 
-  const handleChange = (e) => {
-    const { value, name } = e.target;
-    setUser({ ...user, [name]: value });
-  };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async (data) => {
     try {
-      const { data } = await axios.post(`${process.env.REACT_APP_HEROKU_URL}/users/login`, user);
+      const { data } = await axios.post(`${process.env.REACT_APP_HEROKU_URL}/users/login`,data);
       localStorage.setItem("token", data.data.token)
       localStorage.setItem("email", data.data.email)
-      if (data.data.token) {
-        setTimeout(()=>setLogged(true), 700)
         navigate('/')
-      }
 
 
     } catch (error) {
       console.log(error)
-      alert(`catch error: ${error.response.data}`)
     }
   };
 
@@ -60,6 +38,7 @@ const FormularioLogIn = () => {
       <Nav />
       <div className="loginformAndButtons">
         <div className="head_loginbox">Log in to your Udemy account</div>
+        <div className="container-padding-login">
         <button onClick={()=>loginWithRedirect({connection: 'facebook'})} className="fb-button">
           <img alt="" id="fblogo" src={image1}></img>
           <strong>Continue with Facebook</strong>
@@ -72,58 +51,9 @@ const FormularioLogIn = () => {
           <img alt="" id="glogo" src={image3}></img>
           <strong>Continue with Apple</strong>
         </button>
-
-        <form className="login-form" onSubmit={handleSubmit}>
-          <div className="email">
-            <FaEnvelope />
-            <input
-              id="mail"
-              type="text"
-              placeholder="Email"
-              name="email"
-              onChange={handleChange}
-              value={user.email}
-              /* {...register("email", {
-                required: true,
-                pattern: /[\w]+@{1}[\w]+\.[a-z]{2,3}/,
-              })} */
-            />
-            {/* {errors.email?.type === "required" && <p>Rellene este campo</p>} */}
+          <FormRegisterAndLogin login={true} btn="Log In" Submit={handleSubmit}/>
+          <p className="redirect-signUp">Don't have an account? <Link to="/join/signup">Sign up</Link></p>
           </div>
-          <div className="password">
-            <HiLockClosed />
-            <input
-              id="pass"
-              type="password"
-              placeholder="Password"
-              name='password'
-              onChange={handleChange}
-              value={user.password}
-              /* {...register("password", {
-                required: true,
-              })} */
-            />
-            {/* {errors.password?.type === "required" && <p>Rellene este campo</p>} */}
-          </div>
-          <input className="login-button" type="submit" value="Log In"></input>
-          <div className="fp_na_ol_container">
-            <div className="forgot_pass">
-              or <Link to="/join/forgot-password">Forgot Password</Link>
-            </div>
-            <div className="no_account">
-              Don't have an account?{" "}
-              <Link to="/join/signup">
-                <strong>Sign up</strong>
-              </Link>
-            </div>
-            <div className="organization_login">
-              <Link to="/*">
-                <strong>Log in with your organization</strong>
-              </Link>
-            </div>
-          </div>
-        </form>
-        {logged? <p>Logeado</p> :null }
       </div>
       <Footer />
     </>
